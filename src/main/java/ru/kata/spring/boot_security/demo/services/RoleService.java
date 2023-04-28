86a88d2bdb9fad5bec8d32cc52f95150964ce9da
KIK0M0RI <kudashev.020@gmail.com>
2023-04-28 23:14:35 +0300

    added roleService

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleService.java
@@ -0,0 +1,14 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import ru.kata.spring.boot_security.demo.models.Role;
+
+import java.util.List;
+
+public interface RoleService {
+
+    List<Role> findAll();
+
+    Role findById(long id);
+
+    void save(Role role);
+}
