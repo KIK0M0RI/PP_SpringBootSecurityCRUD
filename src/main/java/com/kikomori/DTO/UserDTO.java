86a68e86aafbce6c02faed6999963351afb9eefc
KIK0M0RI <kudashev.020@gmail.com>
2023-04-29 21:47:48 +0300

    added dto and fix pom

---
+++ src/main/java/com/kikomori/DTO/UserDTO.java
@@ -0,0 +1,24 @@
+package com.kikomori.DTO;
+
+import com.kikomori.models.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private long id;
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+    private String password;
+    private Collection<Role> roles;
+}
