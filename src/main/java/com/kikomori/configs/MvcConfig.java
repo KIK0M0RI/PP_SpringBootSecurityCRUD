d16b75a8bb90dbe5590e2b96823462ac963cfe1b
KIK0M0RI <kudashev.020@gmail.com>
2023-04-29 21:48:32 +0300

    fix MvcConfig

--- src/main/java/com/kikomori/configs/MvcConfig.java
+++ src/main/java/com/kikomori/configs/MvcConfig.java
@@ -1,5 +1,7 @@
 package com.kikomori.configs;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,5 +12,10 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/admin").setViewName("admin");
         registry.addViewController("/user").setViewName("user");
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
